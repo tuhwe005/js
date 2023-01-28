7dc15111ee6446338697231ebf62a0f3a330762d
tuhwe005 <96981681+tuhwe005@users.noreply.github.com>
2023-01-28 20:57:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=argo  # WS 路径前缀。(注意:伪装路径不需要 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=de04add9-5c68-8bab-950c-08cd5320df18
+UUID=d54c2a77-3836-421e-b5bd-dbeeda852ab7
 NEZHA_SERVER=server.nezha.org # 哪吒三个参数，不需要的话可以留空，删除或在这三行最前面加 # 以注释
 NEZHA_PORT=5555
 NEZHA_KEY=olx2IwyG7BZjylaW3H
